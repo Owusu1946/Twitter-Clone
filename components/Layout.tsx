81575cf18b051790313afde6eb2532b60fc876f4
Antonio <hedhogs@gmail.com>
2023-03-06 22:04:48 +0100

    feat: cleanup

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -6,7 +6,7 @@ import Sidebar from "@/components/layout/Sidebar"
 const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
     <div className="h-screen bg-black">
-      <div className="container h-full mx-auto xl:px-30">
+      <div className="container h-full mx-auto xl:px-30 max-w-6xl">
         <div className="grid grid-cols-4 h-full">
           <Sidebar />
           <div 
